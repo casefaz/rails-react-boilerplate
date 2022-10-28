1281afb1b9491c504b1850ed53cf86d70b805a6d
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-10-28 08:52:44 -0600

    Add assocation test to user for inboxes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,15 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
   end
 
+  test '#inboxes' do
+    user = User.create!(name: 'Dill')
+    message1 = Message.create!(content: 'Hey hows it going')
+    message2 = Message.create!(content: 'Open for sales')
+
+    inbox1 = Inbox.create!(user_id: user.id, message_id: message1.id)
+    inbox2 = Inbox.create!(user_id: user.id, message_id: message2.id)
+    # binding.pry
+    assert_equal 2, user.inboxes.size
+  end
+
 end
