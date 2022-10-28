a04935d747cc9b54467ce31987e20565a6998a41
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-10-28 08:58:34 -0600

    Association testing for inbox

--- test/models/inbox_test.rb
+++ test/models/inbox_test.rb
@@ -8,4 +8,26 @@ class InboxTest < ActiveSupport::TestCase
 
     assert inbox1.valid?
   end
+
+  test 'user' do
+    user = User.create!(name: 'Dill')
+    message1 = Message.create!(content: 'Hey hows it going')
+    message2 = Message.create!(content: 'Open for sales')
+
+    inbox1 = Inbox.create!(user_id: user.id, message_id: message1.id)
+    inbox2 = Inbox.create!(user_id: user.id, message_id: message2.id)
+
+    assert inbox1.user.valid?
+  end
+
+  test 'messages' do
+    user = User.create!(name: 'Dill')
+    message1 = Message.create!(content: 'Hey hows it going')
+    message2 = Message.create!(content: 'Open for sales')
+
+    inbox1 = Inbox.create!(user_id: user.id, message_id: message1.id)
+    inbox2 = Inbox.create!(user_id: user.id, message_id: message2.id)
+
+    assert inbox1.message.valid?
+  end
 end
